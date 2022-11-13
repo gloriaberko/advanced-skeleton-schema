6c514af56e15392a617cc8421ab9a424cf2bb8b9
berko <108196454+gloriaberko@users.noreply.github.com>
2022-11-13 17:49:59 -0600

    Add files via upload

---
+++ plugins/example/sh_plugin.lua
@@ -0,0 +1,20 @@
+-- Sets the name for this plugin
+PLUGIN.name = "Example Plugin"
+-- The one who created this plugin
+PLUGIN.author = "Zoephix"
+-- A description to explain what the plugin does,
+-- it would show up in the 'plugins' tab in the F1 'Help' menu if you did do this.
+PLUGIN.desc = "Adds a shared function to check whether the player is in observer."
+-- If you didn't put anything or didn't have it for the PLUGIN.name/author/desc,
+-- it would show as 'Annonymous' for the name of the Author.
+
+local playerMeta = FindMetaTable("Player")
+
+-- Returns whether a player is currently in observer
+function playerMeta:isObserving()
+	if (self:GetMoveType() == MOVETYPE_NOCLIP and !self:InVehicle()) then
+		return true
+	else
+		return false
+	end
+end
