9c9b6767d12b4476c1478bb6437fbeffe62a3aa2
ßerko <108196454+gloriaberko@users.noreply.github.com>
2025-02-24 18:30:04 -0600

    Update sh_plugin.lua

--- plugins/example/sh_plugin.lua
+++ plugins/example/sh_plugin.lua
@@ -1,3 +1,5 @@
+local PLUGIN = PLUGIN
+
 -- Sets the name for this plugin
 PLUGIN.name = "Example Plugin"
 -- The one who created this plugin
@@ -12,7 +14,7 @@ local playerMeta = FindMetaTable("Player")
 
 -- Returns whether a player is currently in observer
 function playerMeta:isObserving()
-	if (self:GetMoveType() == MOVETYPE_NOCLIP and !self:InVehicle()) then
+	if self:GetMoveType() == MOVETYPE_NOCLIP and not self:InVehicle() then
 		return true
 	else
 		return false
